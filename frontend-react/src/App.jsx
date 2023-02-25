a8a8b8f38fa919baa6d57d0523c67bdf87b4de62
Satwik <20h51a0543@cmrcet.ac.in>
2023-02-25 21:43:10 +0530

    add

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,32 +1,65 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+       <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+          <Button variant="primary"><i class="fas fa-moon fa-lg fa-fw"></i></Button>{' '}
+
+          </Nav>
+        </Container>
+      </Navbar>
+      <h1 className="text-center">Hello world </h1>
+      <div style={{
+    display: "flex",
+    alignItems : "center",
+    justifyContent :"center"
+}}> 
+
+
+
+      <InputGroup style={{width:"300px"}} className="mb-3">
+        <Form.Control
+          placeholder="Recipient's username"
+          aria-label="Recipient's username"
+          aria-describedby="basic-addon2"
+        />
+        <Button variant="outline-success" id="button-addon2">
+        <i class="fa fa-add"></i>
+        </Button>
+      </InputGroup>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="mt-4">
+                <Nav justify variant="tabs" defaultActiveKey="?list=BLIND75">
+                    <Nav.Item >
+                        <Nav.Link eventKey="?list=BLIND75">
+                            <span className="tab-font">Todos</span>
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item >
+                        <Nav.Link eventKey="link-1">
+                            <span className="tab-font">Completed</span>
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-2">
+                            <span className="tab-font">Deleted</span>
+                        </Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </div>
     </div>
   )
 }
